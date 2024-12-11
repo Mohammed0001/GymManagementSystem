4eb36e3d5c3c8c86dfce7077ecdbc7ddfab89a68
ALMLYGY <karim219226@bue.edu.eg>
2024-12-11 18:33:50 +0200

    done

--- GymManagementServer/src/main/java/Complaint.java
+++ GymManagementServer/src/main/java/Complaint.java
@@ -18,7 +18,7 @@ public class Complaint {
     private String specialty;
     private GymManager managedBy;
     private ComplaintState compliantState;
-
+    
     public Complaint(int id, String name, Member complainer, String email, String dateSubmitted, String resolutionDate, String status, String specialty, GymManager managedBy, ComplaintState compliantState) {
         this.id = id;
         this.name = name;
@@ -31,83 +31,83 @@ public class Complaint {
         this.managedBy = managedBy;
         this.compliantState = compliantState;
     }
-
+    
     public int getId() {
         return id;
     }
-
+    
     public void setId(int id) {
         this.id = id;
     }
-
+    
     public String getName() {
         return name;
     }
-
+    
     public void setName(String name) {
         this.name = name;
     }
-
+    
     public Member getComplainer() {
         return complainer;
     }
-
+    
     public void setComplainer(Member complainer) {
         this.complainer = complainer;
     }
-
+    
     public String getEmail() {
         return email;
     }
-
+    
     public void setEmail(String email) {
         this.email = email;
     }
-
+    
     public String getDateSubmitted() {
         return dateSubmitted;
     }
-
+    
     public void setDateSubmitted(String dateSubmitted) {
         this.dateSubmitted = dateSubmitted;
     }
-
+    
     public String getResolutionDate() {
         return resolutionDate;
     }
-
+    
     public void setResolutionDate(String resolutionDate) {
         this.resolutionDate = resolutionDate;
     }
-
+    
     public String getStatus() {
         return status;
     }
-
+    
     public void setStatus(String status) {
         this.status = status;
     }
-
+    
     public String getSpecialty() {
         return specialty;
     }
-
+    
     public void setSpecialty(String specialty) {
         this.specialty = specialty;
     }
-
+    
     public GymManager getManagedBy() {
         return managedBy;
     }
-
+    
     public void setManagedBy(GymManager managedBy) {
         this.managedBy = managedBy;
     }
-
+    
     public ComplaintState getCompliantState() {
         return compliantState;
     }
-
+    
     public void setCompliantState(ComplaintState compliantState) {
         this.compliantState = compliantState;
     }
@@ -144,7 +144,7 @@ public class Complaint {
     public void resolve(){
         compliantState.resolve();
     }
-
+    
     @Override
     public String toString() {
         return "Complaint{" + "id=" + id + ", name=" + name + ", complainer=" + complainer + ", email=" + email + ", dateSubmitted=" + dateSubmitted + ", resolutionDate=" + resolutionDate + ", status=" + status + ", specialty=" + specialty + ", managedBy=" + managedBy + ", compliantState=" + compliantState + '}';
