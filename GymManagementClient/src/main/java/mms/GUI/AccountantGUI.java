28a20756866c4fcebbc5d6cc50a4bb9aa0f476f3
ALMLYGY <karim219226@bue.edu.eg>
2024-12-13 20:31:58 +0200

    ALLGUI

--- GymManagementClient/src/main/java/mms/GUI/AccountantGUI.java
+++ GymManagementClient/src/main/java/mms/GUI/AccountantGUI.java
@@ -25,6 +25,7 @@ public class AccountantGUI extends javax.swing.JFrame {
 
         Welcome = new javax.swing.JLabel();
         ManageSubscriptionBTN = new javax.swing.JButton();
+        GenerateReportBTN = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -35,15 +36,24 @@ public class AccountantGUI extends javax.swing.JFrame {
             }
         });
 
+        GenerateReportBTN.setText("Generate financial report");
+        GenerateReportBTN.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                GenerateReportBTNActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(28, 28, 28)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(Welcome, javax.swing.GroupLayout.PREFERRED_SIZE, 341, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(ManageSubscriptionBTN))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(GenerateReportBTN, javax.swing.GroupLayout.DEFAULT_SIZE, 341, Short.MAX_VALUE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(ManageSubscriptionBTN, javax.swing.GroupLayout.DEFAULT_SIZE, 341, Short.MAX_VALUE)
+                        .addComponent(Welcome, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addContainerGap(31, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -53,7 +63,9 @@ public class AccountantGUI extends javax.swing.JFrame {
                 .addComponent(Welcome, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(31, 31, 31)
                 .addComponent(ManageSubscriptionBTN, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(181, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(GenerateReportBTN, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(119, Short.MAX_VALUE))
         );
 
         pack();
@@ -65,7 +77,12 @@ public class AccountantGUI extends javax.swing.JFrame {
         new manageSubscriptionGUI(P).setVisible(true);
     }//GEN-LAST:event_ManageSubscriptionBTNActionPerformed
 
+    private void GenerateReportBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_GenerateReportBTNActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_GenerateReportBTNActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton GenerateReportBTN;
     private javax.swing.JButton ManageSubscriptionBTN;
     private javax.swing.JLabel Welcome;
     // End of variables declaration//GEN-END:variables
